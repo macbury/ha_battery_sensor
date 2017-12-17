6b99bb7903b77a0a096d2d26bb9831713a624b5b
Arkadiusz Buras <macbury@gmail.com>
2017-12-17 19:07:21 +0100

    Working data publishing

--- battery_env_sensor.ino
+++ battery_env_sensor.ino
@@ -1,16 +1,18 @@
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
-
 #include <Wire.h>
+#include <SPI.h>
 #include <Adafruit_Sensor.h>
-#include <Adafruit_BME280.h>
+#include "Adafruit_BME680.h"
 
 #include "settings.c"
 
-#define ESP8266_SDA  4
-#define ESP8266_SCL  2
+#define SEALEVELPRESSURE_HPA (1013.25)
 
-Adafruit_BME280 bme; // I2C
+ADC_MODE(ADC_VCC);
+
+Adafruit_BME680 bme; // I2C
+
 WiFiClient net;
 PubSubClient client(net);
 
@@ -68,20 +70,42 @@ void printWifiInfo() {
   Serial.println(WiFi.localIP());
 }
 
+boolean setupSensor() {
+  if (!bme.begin()) {
+    Serial.println("Could not find a valid BME680 sensor, check wiring!");
+    return false;
+  }
+  bme.setTemperatureOversampling(BME680_OS_8X);
+  bme.setHumidityOversampling(BME680_OS_2X);
+  bme.setPressureOversampling(BME680_OS_4X);
+  bme.setIIRFilterSize(BME680_FILTER_SIZE_3);
+  bme.setGasHeater(320, 150);
+  return bme.performReading();
+}
+
+void publish(char * topic, float value) {
+  char tmp[50];
+  String tmp_str = String(value);
+  tmp_str.toCharArray(tmp, tmp_str.length() + 1);
+  client.publish(topic, tmp, false);
+}
+
+int getBatteryLevel() {
+  return ESP.getVcc();
+}
 
 void setup() {
-  Wire.begin(ESP8266_SDA, ESP8266_SCL);
   Serial.begin(115200);
-  Serial.println(F("BME280 test"));
-
-  if (!bme.begin()) {
-    Serial.println("Could not find a valid BME280 sensor, check wiring!");
-  }
-
   setupWifi();
   if (ensureMqttConnection()) {
-    //setupSensor();
-    client.publish(MQTT_TEMPERATURE_TOPIC, "0");
+    if (setupSensor()) {
+      publish(MQTT_TEMPERATURE_TOPIC, bme.temperature);
+      publish(MQTT_HUMIDITY_TOPIC, bme.humidity);
+      publish(MQTT_PRESSURE_TOPIC, bme.pressure / 100.0);
+      publish(MQTT_GAS_RESISTANCE_TOPIC, bme.gas_resistance / 1000.0);
+      publish(MQTT_ALTITUDE_TOPIC, bme.readAltitude(SEALEVELPRESSURE_HPA));
+      publish(MQTT_VOLTAGE_TOPIC, getBatteryLevel());
+    }
     client.loop();
   }
 
